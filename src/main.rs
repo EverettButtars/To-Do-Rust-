699655b56e4359d0a648f34f91eb9ce43c3ef8f1
Everett Buttars <everettbuttars@gmail.com>
2022-09-30 15:26:12 -0500

    Iced tutorial completed, ready to do my own thing!

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,51 @@
-use iced::{executor, Application, Command, Element, Settings, Text};
+use iced::Settings;
+use iced::pure::Sandbox;
+use iced::pure::widget::{Button, Text, Column, Container};
+
+struct Counter {
+    count: i32
+}
+
+#[derive(Debug, Clone, Copy)]
+enum CounterMessage {
+    Increment,
+    Decrement
+}
 
 pub fn main() -> iced::Result {
-    Hello::run(Settings::default())
+    Counter::run(Settings::default())
  
  }
-struct Hello;
 
 
+impl Sandbox for Counter {
+    type Message = CounterMessage;
 
-impl Application for Hello {
-    type Executor = executor::Default;
-    type Message = ();
-    type Flags = ();
     
-    fn new(_flags: ()) -> (Hello, Command<Self::Message>) {
-        (Hello, Command::none())
+    fn new() -> Self {
+        Counter{ count : 0}
     }
 
     fn title(&self) -> String {
         String::from("To-Do List")
     }
 
-    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
-        Command::none()
+    fn update (&mut self, message: Self::Message) {
+        match message {
+            CounterMessage::Increment => self.count += 1,
+            CounterMessage::Decrement => self.count -= 1
+        }
     }
 
-    fn view(&mut self) -> Element<Self::Message> {
-        Text::new("Hello world!").into()
+    fn view(&self) -> iced::pure::Element<'_, Self::Message> {
+        let label = Text::new(format!("Count: {}", self.count));
+        let incr = Button::new("Increment").on_press(CounterMessage::Increment);
+        let decr = Button::new("Decrement").on_press(CounterMessage::Decrement);
+        
+        let col = Column::new().push(incr).push(label).push(decr);
+
+        Container::new(col).center_x().width(iced::Length::Fill).height(iced::Length::Fill).into()
+        
     }
 
 }
